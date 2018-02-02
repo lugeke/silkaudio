84f0f764e2eb8ef71a78e10ebf76c51adff8c44a
jiaheng <lugeke@gmail.com>
2018-02-02 18:15:22 +0800

    add ssl renew sh

---
+++ renewssl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run --rm --name letsencrypt \  
+    -v "/etc/letsencrypt:/etc/letsencrypt" \
+    -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+    -v "/usr/share/nginx/html:/usr/share/nginx/html" \
+    certbot/certbot:latest \
+    renew --quiet
