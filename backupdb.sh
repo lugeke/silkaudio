9749ef3919a4f65d2747ce4ef45ab285e4e6ad51
jiaheng <lugeke@gmail.com>
2018-02-06 18:08:12 +0800

    mkdir backup and change date format

--- backupdb.sh
+++ backupdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/app/silkaudio/postgresql/backup &&
-docker exec -t silkaudio_postgres_1 pg_dumpall -c -U postgres > ./dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+cd /home/app/silkaudio/postgresql && mkdir -p backup && cd backup && 
+docker exec -t silkaudio_postgres_1 pg_dumpall -c -U postgres > ./dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
