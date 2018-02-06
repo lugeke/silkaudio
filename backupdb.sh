ef5fe0176397b944030290d607cf366953a6e505
jiaheng <lugeke@gmail.com>
2018-02-06 18:03:56 +0800

    cd into backup dir first

--- backupdb.sh
+++ backupdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-docker exec -t silkaudio_postgres_1 pg_dumpall -c -U postgres > ./postgresql/backup/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+cd /home/app/silkaudio/postgresql/backup &&
+docker exec -t silkaudio_postgres_1 pg_dumpall -c -U postgres > ./dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
