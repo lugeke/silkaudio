ed5cc427ef282e31fb3119e0887c13b41c0bb814
jiaheng <lugeke@gmail.com>
2018-02-09 23:08:16 +0800

    fix string multiple line error

--- web/silkaudio/accounts/serializers.py
+++ web/silkaudio/accounts/serializers.py
@@ -35,8 +35,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         """
 
         if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError('''Email already in use, \
-            please use a different email address.''')
+            raise serializers.ValidationError(
+                'Email already in use, please use a different email address.')
 
         return value
 
