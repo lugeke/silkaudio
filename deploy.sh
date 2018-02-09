50850845e4922d334c8b45afa93f5f1c4c9a8925
jiaheng <lugeke@gmail.com>
2018-02-10 00:37:08 +0800

    execute server.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 host=root@138.68.178.137
 scp ./postgresql/settings.env $host:/home/app/silkaudio/postgresql
 scp ./web/settings.env $host:/home/app/silkaudio/web
-# ssh do 'bash -s' < server.sh
+ssh do 'bash -s' < server.sh
